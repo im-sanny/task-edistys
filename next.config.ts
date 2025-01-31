d0fc45282c25502f748de4273f54a3bc74f7577d
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-01-31 21:48:42 +0600

    add: added some assets

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['cdn.sanity.io', 'another-domain.com', 'example.com'],
+  },
 };
 
 export default nextConfig;
